b25da6c1692c2a938b16e0b4aaf7182f3cc393ae
M. Ridwan Fansuri <fansurim@DNID0066L01.local>
2020-02-05 14:41:00 +0700

    initial commit

---
+++ hello_flutter/lib/screens/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.deepPurple,
+      child:  Center(
+        child: Text(
+        sayHello(),
+        textDirection : TextDirection.ltr,
+        style: TextStyle(color: Colors.white, fontSize: 36.0),
+        ),
+      )
+    );
+  }
+
+  String sayHello () {
+    String hello;
+    DateTime now = new DateTime.now();
+    int minute = now.minute;
+    int hour = now.hour;
+    if (hour < 12) {
+      hello = "Good Morning";
+    }
+    else if (hour < 18) {
+      hello = "Good Afternoon";
+    } else {
+      hello = "Good Evening";
+    }
+    String stringMinute = (minute < 10) ? '0' + minute.toString() : minute.toString();
+    return "it's now " + hour.toString() + ":"+ stringMinute + "\n" + hello;
+  }
+}
