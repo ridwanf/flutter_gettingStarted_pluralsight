afa02b94ce3e2c90e1849cb91f797b8b73449551
M. Ridwan Fansuri <fansurim@DNID0066L01.local>
2020-02-06 15:59:06 +0700

    add pizza app

---
+++ flutter_pl/app_widgets/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import "./ui/home.dart";
+
+void main() {
+  runApp(
+    new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Simple Layouts",
+    home: new Home(),
+  ));
+}
+
